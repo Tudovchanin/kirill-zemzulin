e0c625af4eb9ee163af4d55524548bab412482d6
Vlad Halimov <sotto36623@gmail.com>
2025-06-26 20:16:48 +0300

    add delete compression.ts

--- server/middleware/compression.ts
+++
@@ -1,7 +0,0 @@
-import compression from 'compression'
-
-export default defineEventHandler((event) => {
-  const req = event.node.req; // IncomingMessage
-  const res = event.node.res; // ServerResponse
-  compression()(req, res, () => {});
-});
